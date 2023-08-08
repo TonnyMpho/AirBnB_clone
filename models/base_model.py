00fb465ad874be909e02080553f9cd5e4d9d42a6
TonnyMpho <tonnympho1@gmail.com>
2023-08-08 23:13:51 +0200

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,26 @@ BaseModel that defines all common
 attributes/methods for other classes
 """
 
+
 class BaseModel():
     """
     BaseModel that defines all common
     ttributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Class constructor """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.strptime(val, time_format)
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(val, time_format)
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Return string representation of the class """
