6b466898d0d4dcfd496622a24db2e3fdb1c5db36
Pritchad25 <pricts29@gmail.com>
2023-08-13 03:40:04 -0700

    The user class.

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Definition of the User class."""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """This class represents a User instance.
+    Attributes:
+        email (str): the email of the user
+        password (str): the password of the user.
+        first_name (str): the first name of the user.
+        last_name (str): the last name of the user.
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    second_name = ""
