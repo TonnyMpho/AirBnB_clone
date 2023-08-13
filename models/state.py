b6d6ccc1ca58a5cd4dcbe7b6ba14948be8cd7fcb
Pritchad25 <pricts29@gmail.com>
2023-08-13 04:34:18 -0700

    The State class.

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Definition of the State Class."""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """This represents a state.
+
+    Attributes:
+        name (str): the name of the state.
+    """
+
+    name = ""
