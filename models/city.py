2436ca1e0614b561ae36b123d4b8a5b5eb96f47f
Pritchad25 <pricts29@gmail.com>
2023-08-13 08:21:06 -0700

    Changing file to make it pycodestyle compliant.

--- models/city.py
+++ models/city.py
@@ -2,12 +2,14 @@
 """Definition of the City class."""
 from models.base_model import BaseModel
 
-
 class City(BaseModel):
+
     """This class represents a city.
+
     Attributes:
-    state_id (str): the state id.
-    name (str): the name of the city
+        state_id (str): the state id.
+        name (str): the name of the city
     """
+
     state_id = ""
     name = ""
