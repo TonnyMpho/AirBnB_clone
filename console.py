e3764c5f760bb8ef327dac0caedc2eed90936848
TonnyMpho <tonnympho1@gmail.com>
2023-08-13 20:21:31 +0200

    0x00. AirBnB clone - The console

--- console.py
+++ console.py
@@ -117,7 +117,7 @@ class HBNBCommand(cmd.Cmd):
 
         instance = instances[key]
 
-        setattr(instance, args[2], args[3])
+        setattr(instance, args[2].strip('"'), args[3].strip('"'))
         storage.save()
 
     def validate_args(self, args, cls_names):
