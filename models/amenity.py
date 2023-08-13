099e6b9de53dd9d05d7a0d52cca99dc77e89bd1b
Pritchad25 <pricts29@gmail.com>
2023-08-13 07:06:20 -0700

    Making the file pycodestyle compliant.

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Definition of the Amenity Class"""
 from models.base_model import BaseModel
-
 
 class Amenity(BaseModel):
     """This class represents an amenity.
+
     Attributes:
-    name (str): the name of the amenity.
+        name (str): the name of the amenity.
     """
+
     name = ""
