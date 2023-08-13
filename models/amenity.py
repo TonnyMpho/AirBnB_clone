1816920083425d79a4eb0c3eeaef4d302c9d3a4c
Pritchad25 <pricts29@gmail.com>
2023-08-13 04:32:16 -0700

    The Amenity class.

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Definition of the Amenity Class"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """This class represents an amenity.
+    Attributes:
+    name (str): the name of the amenity.
+    """
+    name = ""
