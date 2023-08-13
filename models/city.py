22303bcaa1c99e031083c5f94b479428f19af739
Pritchad25 <pricts29@gmail.com>
2023-08-13 04:33:02 -0700

    The City class.

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Definition of the City class."""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """This class represents a city.
+    Attributes:
+    state_id (str): the state id.
+    name (str): the name of the city
+    """
+    state_id = ""
+    name = ""
